0965cf1fd84ef3161c14256db32cd3a61b00adef
sooshiance <s.katafract@gmail.com>
2024-09-10 00:39:07 +0330

    feat: add hasher function

--- popup.js
+++ popup.js
@@ -8,3 +8,13 @@ document.getElementById("hashButton").addEventListener("click", async () => {
         document.getElementById("hashed").innerHTML = "Please enter a value to hash.";
     }
 });
+
+async function sha256(message) {
+    const msgBuffer = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join("");
+    return hashHex;
+}
